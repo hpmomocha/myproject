86437edf60e2a65ee4e16e26317707eaad879872
hpmomocha <momocha0625@126.com>
2018-05-17 20:14:11 +0800

    add test and static files

--- myfirstproject/boards/tests.py
+++ myfirstproject/boards/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.urls import resolve, reverse
+
+from .views import home
+
+
+class HomeTests(TestCase):
+    def test_home_view_status_code(self):
+        # reverse的第一个参数是urls.py的url中的name
+        url = reverse('home')
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+
+    def test_home_url_resolves_home_view(self):
+        #
+        view = resolve('/')
+        self.assertEquals(view.url_name, 'home')
+        self.assertEquals(view.func, home)
--- myfirstproject/myfirstproject/urls.py
+++ myfirstproject/myfirstproject/urls.py
@@ -13,13 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from boards import views
 from django.conf.urls import url
 from django.contrib import admin
 
-from boards import views
-
 urlpatterns = [
-	# 
-	url(r'^$', views.home, name='home'),
+    #
+    url(r'^$', views.home, name='home'),
     url(r'^admin/', admin.site.urls),
 ]
