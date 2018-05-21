0850a9627c75e4b038d9d89bfda9c74ee0301da9
Kevin <momocha0625@126.com>
2018-05-21 09:56:10 +0800

    user logout

--- myfirstproject/myfirstproject/urls.py
+++ myfirstproject/myfirstproject/urls.py
@@ -18,11 +18,16 @@ from accounts import views as accounts_views
 
 from django.conf.urls import url
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
+
+from myfirstproject import settings
 
 urlpatterns = [
     # views.home是视图函数
     url(r'^$', views.home, name='home'),
     url(r'^signup/$', accounts_views.signup, name='signup'),
+    # url(r'^logout/$', auth_views.LoginView.as_view(), name='logout'),
+    url(r'^logout/$', auth_views.logout, {'next_page': settings.LOGOUT_REDIRECT_URL}, name='logout'),
     url(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
     url(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
     url(r'^admin/', admin.site.urls),
