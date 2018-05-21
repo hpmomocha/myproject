7957823d487ed5bb5efd01c8ce0830b0f36dee5b
hpmomocha <momocha0625@126.com>
2018-05-21 20:12:42 +0800

    用户登录

--- myfirstproject/myfirstproject/urls.py
+++ myfirstproject/myfirstproject/urls.py
@@ -28,6 +28,7 @@ urlpatterns = [
     url(r'^signup/$', accounts_views.signup, name='signup'),
     # url(r'^logout/$', auth_views.LoginView.as_view(), name='logout'),
     url(r'^logout/$', auth_views.logout, {'next_page': settings.LOGOUT_REDIRECT_URL}, name='logout'),
+    url(r'^login/$', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     url(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
     url(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
     url(r'^admin/', admin.site.urls),
