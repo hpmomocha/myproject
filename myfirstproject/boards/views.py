ddd5f7a8eaf7012e479f66fb670f5a1ace9239bc
hpmomocha <momocha0625@126.com>
2018-05-27 11:16:02 +0800

    edit post

--- myfirstproject/boards/views.py
+++ myfirstproject/boards/views.py
@@ -1,7 +1,11 @@
+
+
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.db.models import Count
 from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from django.views.generic import View, UpdateView
 
 from .forms import NewTopicForm, PostForm
 from .models import Board, Topic, Post
@@ -76,3 +80,19 @@ def reply_topic(request, pk, topic_pk):
         form = PostForm()
 
     return render(request, 'reply_topic.html', {'topic': topic, 'form': form})
+
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = ('message',)
+    template_name = 'edit_post.html'
+    pk_url_kwarg = 'post_pk'
+    context_object_name = 'post'
+
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        post.updated_by = self.request.user
+        post.updated_at = timezone.now()
+        post.save()
+        return redirect('topic_posts', pk=post.topic.board.pk, topic_pk=post.topic.pk)
+
--- myfirstproject/myfirstproject/urls.py
+++ myfirstproject/myfirstproject/urls.py
@@ -20,9 +20,16 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 
-from myfirstproject import settings
+from . import settings
 
 urlpatterns = [
+    # views.home是视图函数
+    url(r'^$', views.home, name='home'),
+    url(r'^signup/$', accounts_views.signup, name='signup'),
+    url(r'^login/$', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    url(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
+    # url(r'^logout/$', auth_views.logout, {'next_page': settings.LOGOUT_REDIRECT_URL}, name='logout'),
+
     url(r'^reset/$',
         auth_views.PasswordResetView.as_view(
             template_name='password_reset.html',
@@ -38,21 +45,19 @@ urlpatterns = [
     url(r'^reset/complete/$',
         auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'),
         name='password_reset_complete'),
+
     url(r'^settings/password/$', auth_views.PasswordChangeView.as_view(template_name='password_change.html'),
         name='password_change'),
     url(r'^settings/password/done/$',
         auth_views.PasswordResetDoneView.as_view(template_name='password_change_done.html'),
         name='password_change_done'),
+
     url(r'^boards/(?P<pk>\d+)/topics/(?P<topic_pk>\d+)/$', views.
         topic_posts, name='topic_posts'),
     url(r'^boards/(?P<pk>\d+)/topics/(?P<topic_pk>\d+)/reply/$',
         views.reply_topic, name='reply_topic'),
-    # views.home是视图函数
-    url(r'^$', views.home, name='home'),
-    url(r'^signup/$', accounts_views.signup, name='signup'),
-    # url(r'^logout/$', auth_views.LoginView.as_view(), name='logout'),
-    url(r'^logout/$', auth_views.logout, {'next_page': settings.LOGOUT_REDIRECT_URL}, name='logout'),
-    url(r'^login/$', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    url(r'boards/(?P<pk>\d+)/topics/(?P<topic_pk>\d+)/posts/(?P<post_pk>\d+)/edit/$', views.PostUpdateView.as_view(),
+        name='edit_post'),
     url(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
     url(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
     url(r'^admin/', admin.site.urls),
